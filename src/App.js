7c526b8989faf8023687d1c8b7fad0e4215d9247
Hania-Abdul-Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-12-27 13:52:35 +1030

    adding components directory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // The root component: App returns what looks like html
+    // but its actually JSX/JS Syntax Extension.
+    <div className="container">
+      <h1>Hello from React</h1>
     </div>
   );
 }
