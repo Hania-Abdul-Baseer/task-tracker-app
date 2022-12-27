71fd76b38e500c9b3504d08faa186338209eeae4
Hania-Abdul-Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-12-27 15:18:05 +1030

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-function App() {
+import Header from './components/Header'
+
+const App = () => {
   return (
     // The root component: App returns what looks like html
     // but its actually JSX/JS Syntax Extension.
     <div className="container">
-      <h1>Hello from React</h1>
+      <Header />
     </div>
   );
 }
 
 export default App;
+
